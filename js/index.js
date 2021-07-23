269902d7324ae0746ad0c7aa49a0f065b41dcffa
SangYong-Jeong <mmaf@naver.com>
2021-07-23 11:18:46 +0900

    2교시 끝

--- js/index.js
+++ js/index.js
@@ -1,11 +1,7 @@
 window.addEventListener('scroll', function(e) {
 	var sct = this.scrollY;
 	if(sct > 100) {
-		document.querySelector('.header-wrapper').style.transform = 'translateY(-100%)';
-		this.setTimeout(function() {
-			document.querySelector('.header-wrapper').style.transform = 'translateY(0)';
-			document.querySelector('.header-wrapper').classList.add('active');
-		}, 50)
+		document.querySelector('.header-wrapper').classList.add('active');
 	}
 	else {
 		document.querySelector('.header-wrapper').classList.remove('active');
